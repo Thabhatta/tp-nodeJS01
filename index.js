9b04b6017b3c050b3e9bb5e6658d23f3cc6e3405
“cristianocorrea3” <cristiano.moraes4@etec.sp.gov.br>
2020-09-24 18:41:38 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/* IMPORTAÇÃO DO PACOTE EXPRESS PARA A INDEX */
+// DECLARAR UM CONTANTE
+const express = require('express')
+
+const app = express()
+
+app.listen(app.get('3000'), ()=>{
+	
+	console.log('SERVIDOR RODANDO')
+
+})
